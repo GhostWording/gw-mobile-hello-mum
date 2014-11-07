2112e526cea0e3aead818c52d3917a7e180b7416
Sam Kuhn <dev@samkuhn.net>
2014-11-07 19:43:23 +0000

    feat(config): switch kittens around

--- src/app/config/config.svc.js
+++ src/app/config/config.svc.js
@@ -7,15 +7,15 @@
       'apiUrl': 'http://api.cvd.io/',
       'area' : 'HelloMum', 
       'imageUrls': [
-        'http://images4.fanpop.com/image/photos/16200000/Cute-Little-Kitten-cute-kittens-16288191-1024-768.jpg',
         'http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg',
+        'http://images4.fanpop.com/image/photos/16200000/Cute-Little-Kitten-cute-kittens-16288191-1024-768.jpg',
         'http://cdn.cutestpaw.com/wp-content/uploads/2013/01/l-Cute-Kitten.jpg',
         'http://www.ringof5.com/userdata/fun_rings/123_1361488102_2190747094.jpg'
       ],
-      'imageColours': [
-        'd9d18d',
+      'backgroundColours': [
         '6b6158',
-        '9e4e20',
+        'd9d18d',
+        'ffffff',
         '284061'
       ]
     };
