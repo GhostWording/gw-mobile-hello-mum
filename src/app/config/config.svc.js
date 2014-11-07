32f1aab1da7120224f7e19a2e3406572ce17e878
Sam Kuhn <dev@samkuhn.net>
2014-11-07 18:49:48 +0000

    feat(config): more kittens :)

--- src/app/config/config.svc.js
+++ src/app/config/config.svc.js
@@ -5,11 +5,18 @@
   angular.module('app/config').factory('config', function() {
     return {
       'apiUrl': 'http://api.cvd.io/',
-      'area' : 'Mother', 
-      'intentionSlug': 'happy-birthday',
+      'area' : 'HelloMum', 
       'imageUrls': [
         'http://images4.fanpop.com/image/photos/16200000/Cute-Little-Kitten-cute-kittens-16288191-1024-768.jpg',
-        'http://r.ddmcdn.com/w_622/u_0/gif/05-kitten-cuteness-1.jpg'
+        'http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg',
+        'http://cdn.cutestpaw.com/wp-content/uploads/2013/01/l-Cute-Kitten.jpg',
+        'http://www.ringof5.com/userdata/fun_rings/123_1361488102_2190747094.jpg'
+      ],
+      'imageColours': [
+        'd9d18d',
+        '6b6158',
+        '9e4e20',
+        '284061'
       ]
     };
   });
