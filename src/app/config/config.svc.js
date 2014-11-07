8ede744f3a061d7cf10a980005dabfca0edd7267
Sam Kuhn <dev@samkuhn.net>
2014-11-07 16:04:14 +0000

    chore(config): add image urls

--- src/app/config/config.svc.js
+++ src/app/config/config.svc.js
@@ -6,7 +6,11 @@
     return {
       'apiUrl': 'http://api.cvd.io/',
       'area' : 'Mother', 
-      'intentionSlug': 'happy-birthday'
+      'intentionSlug': 'happy-birthday',
+      'imageUrls': [
+        'http://images4.fanpop.com/image/photos/16200000/Cute-Little-Kitten-cute-kittens-16288191-1024-768.jpg',
+        'http://r.ddmcdn.com/w_622/u_0/gif/05-kitten-cuteness-1.jpg'
+      ]
     };
   });
 
