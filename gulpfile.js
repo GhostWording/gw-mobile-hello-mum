eb71bfd9a12b759e2085455967673c92b43145e2
Sam Kuhn <dev@samkuhn.net>
2014-11-22 15:30:01 +0000

    chore(gulp): glob .mod.js files first from gw-mobile-common

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ var getJSGlobs = function() {
     'src/lib/ngCordova/dist/ng-cordova' + (debug?'':'.min') + '.js',
     'src/lib/gw-common/**/*.js',
     '!src/lib/gw-common/**/*.spec.js',
+    'src/lib/gw-mobile-common/**/*.mod.js',
     'src/lib/gw-mobile-common/**/*.js',
     '!src/lib/gw-mobile-common/**/*.spec.js',
     'src/app/**/*.mod.js',
