8f055cb4177628093b6090b08fc846dbc02bd50b
Sam Kuhn <dev@samkuhn.net>
2014-11-26 12:49:16 +0000

    feat(homescreen): add dev settings popup

--- src/app/home/home.ctrl.js
+++ src/app/home/home.ctrl.js
@@ -2,7 +2,20 @@
 
   "use strict";
 
-  angular.module('app/home').controller('HomeCtrl', function($scope, $cordovaPreferences, config, $window) {
+  angular.module('app/home').controller('HomeCtrl', function($scope, $window, $timeout, $ionicPopup, $cordovaPreferences, config) {
+    // TODO: remove
+    $scope.showDevSettings = function() {
+      $scope.data = {};
+      var devSettingsPopup = $ionicPopup.show({
+        templateUrl: 'app/home/devsettings.part.html',
+        title: 'DEVELOPMENT SETTINGS',
+        scope: $scope,
+        buttons: [
+          { text: 'Close' },
+        ]
+      });
+    };
+    /*
     $scope.mobile = $window.tempMobile;
     $scope.mobileChange = function() {
       // TODO: get $cordovaPreferences to work
@@ -15,6 +28,7 @@
       //$cordovaPreferences.set('email', $scope.email);
       $window.tempEmail = $scope.email;
     };
+    */
   });
 
 }());
