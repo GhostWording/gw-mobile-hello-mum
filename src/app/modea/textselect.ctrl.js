999e52c65ca29787ed91f2530d09be5a7dd855f4
Sam Kuhn <dev@samkuhn.net>
2014-12-02 19:21:45 +0000

    feat(likedislike): remove text on dislike

--- src/app/modea/textselect.ctrl.js
+++ src/app/modea/textselect.ctrl.js
@@ -22,7 +22,7 @@
         var text;
         do {
           text = texts.suggest();
-        } while($scope.textList.indexOf(text) !== -1); 
+        } while($scope.textList.indexOf(text) !== -1 && text.Content.length > 200); 
         $scope.textList.push(text);
       }
     });
@@ -58,6 +58,11 @@
       var currentTextIndex = $scope.textList.indexOf(currentText);
       // If we are on the "come back tomorrow" text
       if(currentTextIndex === -1) {
+        // If there are no texts in the sequence
+        if($scope.textList.length === 0) {
+          // Return null
+          return null;
+        }
         // Return the last item in the sequence
         return $scope.textList[$scope.textList.length-1];
       }
@@ -88,6 +93,11 @@
     };
     // Dislike icon clicked
     $scope.dislikeIconClick = function(item) {
+      // Swipe slide off to left
+      $scope.swipeLeft();
+      // Remove current text from textlist
+      var currentTextIndex = $scope.textList.indexOf($scope.currentText);
+      $scope.textList.splice(currentTextIndex, 1);
     };
   });
 }());
