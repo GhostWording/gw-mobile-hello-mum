2eb57bc376c7df700ae41d17688846b27be4490d
Sam Kuhn <dev@samkuhn.net>
2014-12-09 16:48:57 +0000

    feat(movesettings): automatically progress to modec

--- src/app/home/home.ctrl.js
+++ src/app/home/home.ctrl.js
@@ -3,14 +3,9 @@
   "use strict";
 
   angular.module('app/home').controller('HomeCtrl', function($scope, $window, $location, $timeout, $ionicPopup, $cordovaPreferences, config) {
-    // Next arrow clicked
-    $scope.launch = function() {
-      switch(settings.mode) {
-        case 'A': $location.path('/modea'); break; 
-        case 'B': $location.path('/modeb'); break; 
-        case 'C': $location.path('/modec'); break; 
-      }
-    };
+    $timeout(function() {
+      $location.path('/modec');
+    }, 2000); 
   });
 
 }());
