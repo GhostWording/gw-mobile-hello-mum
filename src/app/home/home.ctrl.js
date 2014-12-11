de2d44a30b87a479f3324e5376be45278f5b768b
Sam Kuhn <dev@samkuhn.net>
2014-12-11 13:38:23 +0000

    feat(home): prevent user from returning to home screen

--- src/app/home/home.ctrl.js
+++ src/app/home/home.ctrl.js
@@ -2,10 +2,15 @@
 
   "use strict";
 
-  angular.module('app/home').controller('HomeCtrl', function($scope, $window, $location, $timeout, $ionicPopup, $cordovaPreferences, config) {
+  angular.module('app/home').controller('HomeCtrl', function($scope, $location, $ionicViewService, $timeout) {
     $timeout(function() {
+      // Prevent user from returning to this screen
+      $ionicViewService.nextViewOptions({
+        disableBack: true
+      });
+      // Go to main view
       $location.path('/textselect');
-    }, 2000); 
+    }, 500); 
   });
 
 }());
