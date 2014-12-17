62714a56ae4055808f1d4da7d0479e1f90682881
Sam Kuhn <dev@samkuhn.net>
2014-12-17 01:48:05 +0000

    feat(pickcontact): fix done button to bottom of device (fixes keyboard problem)

--- src/app/settings/settings.ctrl.js
+++ src/app/settings/settings.ctrl.js
@@ -2,7 +2,11 @@
 
   "use strict";
 
-  angular.module('app/settings').controller('SettingsCtrl', function($scope) {
+  angular.module('app/settings').controller('SettingsCtrl', function($window, $scope) {
+    // Get device width and height
+    // TODO: move into service
+    $scope.deviceWidth = $window.deviceWidth;    
+    $scope.deviceHeight = $window.deviceHeight;    
   });
 
 }());
