8a46e863bade6c1c7d0223df8a7cad6fb33483df
Sam Kuhn <dev@samkuhn.net>
2014-12-22 20:05:41 +0000

    feat(localnotification): add timepicker to notification tab

--- src/app/settings/settings.ctrl.js
+++ src/app/settings/settings.ctrl.js
@@ -7,6 +7,20 @@
     // TODO: move into service
     $scope.deviceWidth = $window.deviceWidth;    
     $scope.deviceHeight = $window.deviceHeight;    
+    // Notifications
+    $scope.notify = {
+      hour: 12,
+      minute: 45
+    };
+    $scope.$watch('notify.hour', function() {
+      setNotification();
+    });
+    $scope.$watch('notify.minute', function() {
+      setNotification();
+    });
+    // Update notification
+    function setNotification() {
+    }
   });
 
 }());
--- src/app/settings/settings.mod.js
+++ src/app/settings/settings.mod.js
@@ -3,6 +3,7 @@
   "use strict";
 
   angular.module('app/settings', [
+    'mobile/timepicker'
   ]);
 
 }());
