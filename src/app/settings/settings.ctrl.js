0a983448343992227b58f5eda91abd5b7df5a4a1
Sam Kuhn <dev@samkuhn.net>
2014-12-30 22:42:27 +0000

    feat(intentionfrequency): show all intentions and automatically rendrer/persist from json data

--- src/app/settings/settings.ctrl.js
+++ src/app/settings/settings.ctrl.js
@@ -7,6 +7,20 @@
     // TODO: move into service
     $scope.deviceWidth = $window.deviceWidth;    
     $scope.deviceHeight = $window.deviceHeight;    
+    // Intentions
+    $scope.intentions = [
+      {name: 'how-are-you', label:'How Are You'},
+      {name: 'I-think-of-you', label:'Thinking Of You'},
+      {name: 'jokes', label:'Jokes'},
+      {name: 'thank-you', label:'Thank You'},
+      {name: 'a-few-words-for-you', label:'A Few Words For You'},
+      {name: 'I-love-you', label:'I Love You'},
+      {name: 'I-miss-you', label:'I Miss You'},
+      {name: 'I-am-here-for-you', label:'Here For You'},
+      {name: 'facebook-status', label:'Facebook Status'},
+      {name: 'positive-thoughts', label:'Poisitive Thoughts'},
+      {name: 'would-you-care-for-a-drink', label:'Care For A Drink?'}
+    ];
     // Watch notification toggle
     $scope.$watch('settings.notification', function(notification) {
       if(notification) {
