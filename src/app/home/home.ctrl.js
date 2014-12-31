e692b3e3beaa3dcc982fa627bfab206b3ec90332
Sam Kuhn <dev@samkuhn.net>
2014-12-31 16:48:38 +0000

    feat(welcometexts): load welcome texts before progressing

--- src/app/home/home.ctrl.js
+++ src/app/home/home.ctrl.js
@@ -2,20 +2,26 @@
 
   "use strict";
 
-  angular.module('app/home').controller('HomeCtrl', function($scope, $location, $ionicViewService, $timeout, settings) {
+  angular.module('app/home').controller('HomeCtrl', function($scope, $location, $ionicViewService, $timeout, settings, texts) {
     $timeout(function() {
       // Prevent user from returning to this screen
       $ionicViewService.nextViewOptions({
         disableBack: true
       });
-      // If we dont know the users gender
-      if(!settings.userGender) {
-        // Go to gender select
-        $location.path('/genderselect');
-      } else {
-        // Go to text select
-        $location.path('/textselect');
-      }
+      // Fetch welcome texts
+      texts.fetchWelcome().then(function() {
+        // If we dont know the users gender
+        if(!settings.userGender) {
+          // Go to gender select
+          $location.path('/genderselect');
+        } else {
+          // Go to text select
+          $location.path('/textselect');
+        }
+      }, function() {
+        // TODO: improve this
+        alert('no internet connectivity');
+      });
     }, 500); 
   });
 
