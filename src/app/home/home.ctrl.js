4b0b663d5071555048e2a9a238730e2f5dd6476b
Sam Kuhn <dev@samkuhn.net>
2015-01-29 00:50:18 +0000

    feat(popupgenderselect): go straight to textselect screen

--- src/app/home/home.ctrl.js
+++ src/app/home/home.ctrl.js
@@ -2,7 +2,7 @@
 
   "use strict";
 
-  angular.module('app/home').controller('HomeCtrl', function($scope, $location, $ionicViewService, $timeout, settings, texts, localisation, analytics) {
+  angular.module('app/home').controller('HomeCtrl', function($scope, $location, $ionicViewService, $timeout, texts, localisation, analytics) {
     $timeout(function() {
       // Report home page init
       analytics.reportEvent('Init', 'Page', 'Home', 'Init');        
@@ -14,14 +14,8 @@
       texts.setLanguage(localisation.getLanguage().split('-')[0]);
       // Fetch welcome texts
       texts.fetchWelcome().then(function() {
-        // If we dont know the users gender
-        if(!settings.userGender) {
-          // Go to gender select
-          $location.path('/genderselect');
-        } else {
-          // Go to text select
-          $location.path('/textselect');
-        }
+        // Go to text select
+        $location.path('/textselect');
       }, function() {
         // TODO: improve this
         alert('no internet connectivity');
