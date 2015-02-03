33ada116613e08952d28e94200db7ca2b7ffa0fa
Sam Kuhn <dev@samkuhn.net>
2015-02-03 20:53:45 +0000

    fix(splash): dynamic year in copyright text

--- src/app/splash/splash.ctrl.js
+++ src/app/splash/splash.ctrl.js
@@ -3,13 +3,16 @@
   "use strict";
 
   angular.module('app/splash').controller('SplashCtrl', function($scope, $location, $ionicViewService, $timeout, texts, localisation, analytics) {
+    // Report splash page init
+    analytics.reportEvent('Init', 'Page', 'Splash', 'Init');        
+    // Prevent user from returning to this screen
+    $ionicViewService.nextViewOptions({
+      disableBack: true
+    });
+    // Get year for copyright text
+    $scope.year = (new Date()).getFullYear(); 
+    // Wait a bit
     $timeout(function() {
-      // Report splash page init
-      analytics.reportEvent('Init', 'Page', 'Splash', 'Init');        
-      // Prevent user from returning to this screen
-      $ionicViewService.nextViewOptions({
-        disableBack: true
-      });
       // Fetch welcome texts
       texts.fetchWelcome().then(function() {
         // Go to text select
