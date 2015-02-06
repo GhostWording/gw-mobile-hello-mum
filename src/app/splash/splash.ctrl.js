d7f2f1dcf5f3cf58504be414dd5e411c12bcac35
Sam Kuhn <dev@samkuhn.net>
2015-02-06 01:27:58 +0000

    feat(connectivity): move text fetch code from textselect and display spinner

--- src/app/splash/splash.ctrl.js
+++ src/app/splash/splash.ctrl.js
@@ -2,7 +2,7 @@
 
   "use strict";
 
-  angular.module('app/splash').controller('SplashCtrl', function($scope, $location, $ionicViewService, $timeout, texts, localisation, analytics) {
+  angular.module('app/splash').controller('SplashCtrl', function($scope, $location, $ionicViewService, $timeout, texts, localisation, analytics, config, settings) {
     // Report splash page init
     analytics.reportEvent('Init', 'Page', 'Splash', 'Init');        
     // Prevent user from returning to this screen
@@ -13,15 +13,58 @@
     $scope.year = (new Date()).getFullYear(); 
     // Wait a bit
     $timeout(function() {
-      // Fetch welcome texts
-      texts.fetchWelcome().then(function() {
-        // Go to text select
-        $location.path('/textselect');
-      }, function() {
-        // TODO: improve this
-        alert('no internet connectivity');
-      });
+      // Attempt to fetch texts
+      fetchTexts(); 
     }, 1500); 
+    // On app return to foreground
+    document.addEventListener("resume", function() {
+      // Hide connectivity message
+      $scope.showConnectivityMessage = false;
+      // Try fetching welcome texts again
+      fetchTexts();
+      // Apply since we are not in angular world
+      $scope.$apply();
+    }, false);
+    // Fetch texts from server
+    function fetchTexts() {
+      // If we have shown the welcome texts (n) times
+      // NOTE: no welcome texts for spanish language
+      if(localisation.getLanguage() === 'es' || 
+        (settings.welcomeTextsShownCount !== undefined && 
+        settings.welcomeTextsShownCount >= config.showWelcomeTextTimes)) {
+        // Trigger a fetch of all texts
+        texts.fetch().then(function() {
+          // Pick from all texts
+          texts.useWelcome(false);
+          // Good to go..
+          $location.path('/textselect');
+        }, function() {
+          // Failed to fetch texts
+          $scope.showConnectivityMessage = true;
+        });
+      } else {
+        // Fetch welcome texts
+        texts.fetchWelcome().then(function() {
+          // Flag welcome texts as shown
+          if(settings.welcomeTextsShownCount === undefined) {
+            settings.welcomeTextsShownCount = 1;
+          } else {
+            settings.welcomeTextsShownCount ++;
+          }
+          // Save settings
+          settings.save(); 
+          // Trigger a fetch of all texts in the background
+          //texts.fetch();
+          // Pick from welcome texts
+          texts.useWelcome(true);
+          // Good to go..
+          $location.path('/textselect');
+        }, function() {
+          // Failed to fetch texts
+          $scope.showConnectivityMessage = true;
+        });
+      }
+    }
   });
 
 }());
