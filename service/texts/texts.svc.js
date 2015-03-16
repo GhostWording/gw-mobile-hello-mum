a20cf173b7751029c4fa20efef2276788d4d5678
Sam Kuhn <dev@samkuhn.net>
2015-03-16 12:38:16 +0000

    feat(fixwelcome): add welcome texts back in

--- service/texts/texts.svc.js
+++ service/texts/texts.svc.js
@@ -7,15 +7,19 @@
     var _weightedIntentions;
     var _textLists;
     var _welcomeTextList;
+    // TODO: How to simplify this file? It's a bit hairy!
+    // TODO: It would be good to generalise it (if possible?) and move into gw-mobile-common
+    // TODO: It would be nice not to rely on hello mum specific code in gw-common too
     var texts = {
       // Fetch texts from server (or cache)
       fetch: function() {
-        _useWelcome = false;
         // Text fetch retry function
         function fetchTexts() {
           // Fetch texts
-          return fetchAll()
+          return (_useWelcome?fetchWelcome():fetchAll())
             .then(function() {
+              // Trigger a fetch of all texts in the background
+              if(_useWelcome) fetchAll();
               // Hide loading overlay
               $rootScope.loading = false;
             }, function() {
@@ -31,50 +35,26 @@
         }
         // Show loading overlay
         $rootScope.loading = true;
-        // Fetch texts (with retry)
-        return fetchTexts();
-        /*
-        // If we have shown the welcome texts (n) times
-        // NOTE: no welcome texts for spanish language
-        if(localisation.getLanguage() === 'es' || 
-          (settings.welcomeTextsShownCount !== undefined && 
-          settings.welcomeTextsShownCount >= config.showWelcomeTextTimes)) {
-          // Trigger a fetch of all texts
-          fetchAll().then(function() {
-            // Pick from all texts
-            _useWelcome = false;
-          }, function() {
-            // Failed to fetch texts
-            $scope.showConnectivityMessage = true;
-            // Retry
-            $timeout(fetchTexts, config.textFetchRetryDelay * 1000);
-          });
-        } else {
-          // Fetch welcome texts
-          texts.fetchWelcome().then(function() {
-            // Flag welcome texts as shown
+        // Decide whether to fetch welcome texts
+        // NOTE: no welcome texts in spanish version
+        if(localisation.getLanguage() !== 'es' &&
+          (settings.welcomeTextsShownCount === undefined || 
+          settings.welcomeTextsShownCount < config.showWelcomeTextTimes)) {
+            // Use welcome texts
+            _useWelcome = true;
+            // Increment welcome text show count
             if(settings.welcomeTextsShownCount === undefined) {
               settings.welcomeTextsShownCount = 1;
             } else {
               settings.welcomeTextsShownCount ++;
             }
-            // Save settings
-            settings.save(); 
-            // Trigger a fetch of all texts in the background
-            texts.fetch();
-            // Pick from welcome texts
-            _useWelcome = true;
-            // Good to go..
-            $state.go('home');
-          }, function() {
-            // Failed to fetch texts
-            $scope.showConnectivityMessage = true;
-            // Retry
-            $timeout(fetchTexts, config.textFetchRetryDelay * 1000);
-          });
+        } else {
+          // Use all texts
+          _useWelcome = false;
         }
-        */
-      },
+        // Fetch texts (with retry)
+        return fetchTexts();
+       },
       // Choose (n) texts
       choose: function(n) {
         // If welcome text flash
@@ -113,21 +93,10 @@
         }
         return intentions;
       },
-      // Clear cached texts
-      clear: function() {
-        /*
-        for(var i=0; i<_weightedIntentions.length; i++) {
-          var intentionName = _weightedIntentions[i].name;
-          console.log('HelloMum/' + intentionName + '/texts.en-EN');
-          cacheSvc.reInitializeCacheEntry('HelloMum/' + intentionName + '/texts.en-EN');
-        }
-        */
-      },
       remove: function(text) {
         _textList.splice(_textList.indexOf(text, 1));
       }
     };
-
     // Fetch welcome text list
     function fetchWelcome() {
       // Set user gender
@@ -138,7 +107,6 @@
         return textList;
       }); 
     }
- 
     // Fetch text list
     function fetchAll() {
       // Set user gender
@@ -155,7 +123,7 @@
         return _textLists;
       });
     }
-
+    // Get a map of weighted intentions
     function getWeightedIntentions() {
       // Get slugs and default weights for hello mum intentions
       var weightedIntentions = helloMumSvc.intentionDefaultWeights();
@@ -178,8 +146,7 @@
       helloMumSvc.setIntentionWeights(weightedIntentions);
       return weightedIntentions;
     }
-
-    // Will try to pick a random text until satisfied
+    // Choose a random text
     function chooseText(weightedIntentions) {
       var choice;
       var happyWithChoice = true;
@@ -188,12 +155,10 @@
       } while (happyWithText(choice.text) === false);
       return choice;
     }
-
-    // Let us say that 'burnt' texts are texts disliked + texts already sent
-    // We don't want them
-
-    // TODO : if chosen text is burnt, express deep sadness
+    // Text selection criteria
     function happyWithText(pickedText) {
+      // Let us say that 'burnt' texts are texts disliked + texts already sent
+      // We don't want them
       // TODO : check that text has not been picked during this session
       // if matching text Id in $scope.choices, return false
       // TODO : check that text has not been disliked previously
