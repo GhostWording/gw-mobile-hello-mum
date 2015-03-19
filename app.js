b722891798df0c1d968614f715060cfaadbfe0dd
Sam Kuhn <dev@samkuhn.net>
2015-03-19 01:17:23 +0000

    feat(demumify): rename mumPetName service

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
     .run(function(
       /* 3RD */ $ionicPlatform, $translate, 
       /* GMC */ config, settings, notification,
-      /* APP */ mumPetName) {
+      /* APP */ petName) {
       $ionicPlatform.ready(function() {
         // Set notification
         if(settings.notification) {
           $translate('NOTIFICATION').then(function (notificationText) {
-            notification.set(settings.notificationHour, settings.notificationMinute, mumPetName.replace(notificationText, settings.mumPetName));
+            notification.set(settings.notificationHour, settings.notificationMinute, petName.replace(notificationText, settings.petName));
           });
         } else {
           notification.clear();
