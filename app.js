d5ebf019e05fedd440b86839a6fcd89b77aef7bb
Sam Kuhn <dev@samkuhn.net>
2015-03-19 11:22:56 +0000

    feat(demumify): pick first pet name if no preference

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
       /* GMC */ config, settings, notification,
       /* APP */ petName) {
       $ionicPlatform.ready(function() {
+        // If pet name hasn't been chosen, use the first in the list
+        if(settings.petName === undefined) settings.petName = config.petNames[0];
         // Set notification
         if(settings.notification) {
           $translate('NOTIFICATION').then(function (notificationText) {
